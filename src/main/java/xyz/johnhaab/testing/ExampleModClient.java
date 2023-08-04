7fe9d7fc9432a00e4b21528a768b7391ceed5b84
johnhaab <wearedevelopersbiz@gmail.com>
2023-08-04 00:09:07 -0400

    First commit

---
+++ src/main/java/xyz/johnhaab/testing/ExampleModClient.java
@@ -0,0 +1,11 @@
+package xyz.johnhaab.testing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
